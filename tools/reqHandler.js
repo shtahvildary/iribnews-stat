ba394bbc8ee9b15ef9bf3b13f09950d7671964d9
shtahvildary <shtahvildary@gmail.com>
2018-02-18 15:36:51 +0330

    change bot for test

--- tools/reqHandler.js
+++ tools/reqHandler.js
@@ -1,8 +1,11 @@
 var request=require("request");
+//admin it-word cloud
+var url="https://api.telegram.org/bot456299862:AAGB1q_AMolsLpeE5EARolW4FHEi5-1kqjE/";
+
 //shmt_bot
 // var url="https://api.telegram.org/bot449968526:AAGY4Tz48MiN8uxUD_0nWHFZSQscD9OQ_Vk/";
 //iribnewsbot
-var url="https://api.telegram.org/bot545443179:AAGEKFAT_mg5H2aTZbCKEPXr2Pkee11b8l4/";
+// var url="https://api.telegram.org/bot545443179:AAGEKFAT_mg5H2aTZbCKEPXr2Pkee11b8l4/";
 
 var req=function(method,reqBody,callback){
   console.log(method)
